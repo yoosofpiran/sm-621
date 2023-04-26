6c8184238097b80eb9ae32304b7053ba4cc4cb53
Joseph <yoosofpiran@gmail.com>
2023-04-27 00:12:53 +0330

    Update sm621.py

--- sm621.py
+++ sm621.py
@@ -3,90 +3,77 @@ import threading
 import tkinter as tk
 from tkinter import ttk
 
-class SerialGUI:
+class SM621GUI:
     def __init__(self, master):
         self.master = master
-        self.master.title("Serial GUI")
+        master.title("SM621 Serial Reader")
 
-        # Initialize serial port
+        # Create Serial Port Label and Combo Box
+        self.serial_port_label = ttk.Label(master, text="Serial Port:")
+        self.serial_port_label.grid(column=0, row=0, padx=5, pady=5)
+
+        self.serial_port_combo = ttk.Combobox(master, values=self.serial_ports())
+        self.serial_port_combo.grid(column=1, row=0, padx=5, pady=5)
+        self.serial_port_combo.current(0)
+
+        # Create Baud Rate Label and Combo Box
+        self.baud_rate_label = ttk.Label(master, text="Baud Rate:")
+        self.baud_rate_label.grid(column=0, row=1, padx=5, pady=5)
+
+        self.baud_rate_combo = ttk.Combobox(master, values=["2400", "4800", "9600", "19200", "38400", "57600", "115200"])
+        self.baud_rate_combo.grid(column=1, row=1, padx=5, pady=5)
+        self.baud_rate_combo.current(2)
+
+        # Create Open and Close Serial Port Buttons
+        self.open_button = ttk.Button(master, text="Open Port", command=self.open_serial_port)
+        self.open_button.grid(column=0, row=2, padx=5, pady=5)
+
+        self.close_button = ttk.Button(master, text="Close Port", command=self.close_serial_port, state="disabled")
+        self.close_button.grid(column=1, row=2, padx=5, pady=5)
+
+        # Create Send Command Button
+        self.send_command_button = ttk.Button(master, text="Send Command", command=self.send_command, state="disabled")
+        self.send_command_button.grid(column=2, row=2, padx=5, pady=5)
+
+        # Create Text Box to Display Received Data
+        self.text_box = tk.Text(master, width=50, height=10)
+        self.text_box.grid(column=0, row=3, columnspan=3, padx=5, pady=5)
+
+        # Set up serial port object
         self.ser = None
+        self.ser_lock = threading.Lock()
 
-        # Initialize GUI elements
-        self.port_label = ttk.Label(master, text="Port:")
-        self.port_label.grid(row=0, column=0)
+        # Set up thread for reading serial data
+        self.thread = threading.Thread(target=self.serial_reader_thread)
+        self.thread_stop_event = threading.Event()
 
-        self.port_entry = ttk.Entry(master)
-        self.port_entry.grid(row=0, column=1)
+    def serial_ports(self):
+        """ Lists serial port names """
+        ports = ['COM{}'.format(i + 1) for i in range(256)]
+        result = []
+        for port in ports:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                result.append(port)
+            except (OSError, serial.SerialException):
+                pass
+        return result
 
-        self.baudrate_label = ttk.Label(master, text="Baudrate:")
-        self.baudrate_label.grid(row=1, column=0)
+    def open_serial_port(self):
+        """ Opens the selected serial port """
+        port = self.serial_port_combo.get()
+        baud_rate = self.baud_rate_combo.get()
 
-        self.baudrate_entry = ttk.Entry(master)
-        self.baudrate_entry.insert(0, "9600")
-        self.baudrate_entry.grid(row=1, column=1)
-
-        self.open_button = ttk.Button(master, text="Open", command=self.open_port)
-        self.open_button.grid(row=2, column=0)
-
-        self.close_button = ttk.Button(master, text="Close", command=self.close_port)
-        self.close_button.grid(row=2, column=1)
-
-        self.send_button = ttk.Button(master, text="Send", command=self.send_command)
-        self.send_button.grid(row=3, column=0)
-
-        self.receive_label = ttk.Label(master, text="Received Data:")
-        self.receive_label.grid(row=4, column=0)
-
-        self.receive_text = tk.Text(master, height=10, state="disabled")
-        self.receive_text.grid(row=5, column=0, columnspan=2)
-
-        # Initialize thread for receiving data
-        self.thread = threading.Thread(target=self.receive_data, daemon=True)
-
-    def open_port(self):
-        if self.ser:
-            self.ser.close()
-
-        port = self.port_entry.get()
-        baudrate = int(self.baudrate_entry.get())
-
-        try:
-            self.ser = serial.Serial(port, baudrate)
-            self.thread.start()
-            self.open_button.configure(state="disabled")
-            self.close_button.configure(state="normal")
-            self.send_button.configure(state="normal")
-            self.port_entry.configure(state="disabled")
-            self.baudrate_entry.configure(state="disabled")
-        except serial.SerialException as e:
-            tk.messagebox.showerror("Error", str(e))
-
-    def close_port(self):
-        if self.ser:
-            self.ser.close()
-            self.thread.join()
-            self.open_button.configure(state="normal")
-            self.close_button.configure(state="disabled")
-            self.send_button.configure(state="disabled")
-            self.port_entry.configure(state="normal")
-            self.baudrate_entry.configure(state="normal")
-
-    def send_command(self):
-        if self.ser:
-            command = bytes([0xc0, 0x1, 0xff, 0xff, 0xff, 0xff,0xc0])
-            self.ser.write(command)
-
-    def receive_data(self):
-        while True:
-            if self.ser:
-                try:
-                    data = self.ser.read_until(bytes([0xc0]))
-                    self.receive_text.configure(state="normal")
-                    self.receive_text.insert("end", data.hex() + "\n")
-                    self.receive_text.configure(state="disabled")
-                except serial.SerialException:
-                    pass
-
-root = tk.Tk()
-app = SerialGUI(root)
-root.mainloop()
+        if not self.ser:
+            try:
+                self.ser = serial.Serial(port, baud_rate)
+                self.open_button.config(state="disabled")
+                self.close_button.config(state="enabled")
+                self.send_command_button.config(state="enabled")
+                self.thread_stop_event.clear()
+                self.thread.start()
+            except serial.SerialException as e:
+                self.text_box.insert(tk.END, "Error opening serial port: {}\n".format(str(e)))
+        else:
+            self
